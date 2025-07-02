f50d76f0e1366a8ea6f257442114982d36ae7ab6
lorelairaras <lorelairaras@gmail.com>
2025-07-02 17:50:39 +0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form");
+
+function handleSubmit(e) {
+  e.preventDefault(); // prevent the default behaviour
+}
+
+form.addEventListener("submit", handleSubmit);
